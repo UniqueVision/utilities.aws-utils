4111cdbc29e6f60d09c4341bdda6eca993078b01
Kouhei Aoyagi <aoyagi_kouhei@uniquevision.co.jp>
2025-07-29 13:09:03 +0900

    add is_conditional_check_failed_exception

--- crates/dynamodb/CHANGELOG.md
+++ crates/dynamodb/CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changes
 
+## v0.1.1 (2025/07/29)
+* add is_conditional_check_failed_exception
+
 ## v0.1.0 (2025/07/29)
 * first release
--- crates/dynamodb/src/error.rs
+++ crates/dynamodb/src/error.rs
@@ -21,3 +21,17 @@ pub enum Error {
 pub(crate) fn from_aws_sdk_error(e: impl Into<aws_sdk_dynamodb::Error>) -> Error {
     Error::AwsSdk(Box::new(e.into()))
 }
+
+impl Error {
+    pub fn is_conditional_check_failed_exception(&self) -> bool {
+        match self {
+            Error::AwsSdk(e) => {
+                match e.as_ref() {
+                    aws_sdk_dynamodb::Error::ConditionalCheckFailedException(_) => true,
+                    _ => false,
+                }
+            },
+            _ => false,
+        }
+    }
+}
